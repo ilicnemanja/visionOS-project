b558a8ac9a83c65b43f1b4cdcbd2fc4e8d2096de
Nemanja <ilicnemanja.it@gmail.com>
2024-07-19 14:41:55 +0200

    added firebase config

--- HappyBeam/Views/LeaderboardService.swift
+++ HappyBeam/Views/LeaderboardService.swift
@@ -1,5 +1,5 @@
-//import FirebaseFirestore
-//import Combine
+import FirebaseFirestore
+import Combine
 
 struct ScoreEntry: Identifiable {
     let id: String
@@ -8,28 +8,28 @@ struct ScoreEntry: Identifiable {
 }
 
 class LeaderboardService {
-//    private let db = Firestore.firestore()
-    // @Published var leaderboard: [ScoreEntry] = []
+    private let db = Firestore.firestore()
+     @Published var leaderboard: [ScoreEntry] = []
 
     func addScore(email: String, score: Int, completion: @escaping (Error?) -> Void) {
         let newScore = ["email": email, "score": score] as [String : Any]
         print("New Score: ", newScore)
-//        db.collection("leaderboard").addDocument(data: newScore, completion: completion)
+        db.collection("leaderboard").addDocument(data: newScore, completion: completion)
     }
 
-//    func fetchLeaderboard() {
-//        db.collection("leaderboard").order(by: "score", descending: true).limit(to: 10).addSnapshotListener { snapshot, error in
-//            if let error = error {
-//                print("Error fetching leaderboard: \(error)")
-//            } else {
-//                self.leaderboard = snapshot?.documents.compactMap { doc -> ScoreEntry? in
-//                    let data = doc.data()
-//                    let id = doc.documentID
-//                    let email = data["email"] as? String ?? ""
-//                    let score = data["score"] as? Int ?? 0
-//                    return ScoreEntry(id: id, email: email, score: score)
-//                } ?? []
-//            }
-//        }
-//    }
+    func fetchLeaderboard() {
+        db.collection("leaderboard").order(by: "score", descending: true).limit(to: 10).addSnapshotListener { snapshot, error in
+            if let error = error {
+                print("Error fetching leaderboard: \(error)")
+            } else {
+                self.leaderboard = snapshot?.documents.compactMap { doc -> ScoreEntry? in
+                    let data = doc.data()
+                    let id = doc.documentID
+                    let email = data["email"] as? String ?? ""
+                    let score = data["score"] as? Int ?? 0
+                    return ScoreEntry(id: id, email: email, score: score)
+                } ?? []
+            }
+        }
+    }
 }
